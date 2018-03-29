9dd949074e813e08604d1f50d479544ba9eb1fcc
Dan Rosen <drosen84@gmail.com>
2018-03-29 14:47:30 -0500

    ran successful dummy test

---
+++ test/test-polling.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const faker = require('faker');
+const mongoose = require('mongoose');
+const app = require('../server.js');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('index page', function () {
+  it('should exist', function () {
+    return chai.request(app)
+      .get('/')
+      .then(function (res) {
+        expect(res).to.have.status(200);
+      });
+  });
+});
+
+
+
+
+
