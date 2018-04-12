f58d4ea9fa7b1bc0edce667e2c2aac2bca7305b7
Dan Rosen <drosen84@gmail.com>
2018-04-12 11:04:35 -0500

    added search boxes to dashboard.html file. not yet hooked up. deleted unneeded poll.js file.

--- config.js
+++ config.js
@@ -7,3 +7,4 @@ exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhos
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET ;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+// const config = {API_KEY : 'wnY4Trg2G2BzWvRBHlMLl-_1KEt2Z7Hd-2D4untSR_KJ6fJYwhAlT_Y-InkvpNrfq21EKtBsJD_G2aC0DUnwQEBRO43pX4zbR39mTNe8-NFwZp_GOEF6tuDS0fLLWnYx'};
---
+++ public/api.js
@@ -0,0 +1,18 @@
+'use strict';
+/* global $ config */
+
+const yelpBaseUrl = 'https://api.yelp.com/v3/businesses';
+const apiKey = 'wnY4Trg2G2BzWvRBHlMLl-_1KEt2Z7Hd-2D4untSR_KJ6fJYwhAlT_Y-InkvpNrfq21EKtBsJD_G2aC0DUnwQEBRO43pX4zbR39mTNe8-NFwZp_GOEF6tuDS0fLLWnYx';
+
+const api = function() {
+  return {
+    randomInteger: function(max) {
+      return Math.floor(Math.random() * Math.floor(max));
+    },
+    getBusinessDataFromYelpApi: function(searchTerm, callback, apiKey) {
+      const query = {
+
+      };
+    },
+  };
+};
