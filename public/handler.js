48c945623aef65d8495bda8794bf4f8bfedf772e
Dan Rosen <drosen84@gmail.com>
2018-05-08 14:39:01 -0500

    began merging dashboard html into index.html file. also starting writing up view switching in generator.js function

--- public/handler.js
+++ public/handler.js
@@ -1,6 +1,41 @@
 'use strict';
 /* global generator api $ */
 
+const STORE = {};
+STORE.currentView = 'login';
+
+switch (STORE.currentView) {
+  case 'login':
+    ids_to_show.concat(['.js-login-form login-form', '.js-signup-form']);
+    ids_to_hide.concat(['.js-generated-poll-results', '.js-existing-poll', '.js-poll-creation']);  
+  case 'dashboard':
+    ids_to_show.concat(['.js-poll-creation']);
+    ids_to_hide.concat(['.js-generated-poll-results', '.js-existing-poll', '.js-login-form login-form', '.js-signup-form']);
+    break;
+  case 'poll-create':
+    ids_to_show.concat(['.js-generated-poll-results']);
+    ids_to_hide.concat(['.js-poll-creation', '.js-existing-poll', '.js-login-form login-form', '.js-signup-form']);
+    break;
+  
+
+};
+ 
+  showAll(ids_to_show);
+  hideAll(ids_to_hide);
+
+  function showAll(array) {
+    array.forEach((id) => {
+      id.show();
+    });
+
+    function hideAll(array) {
+    array.forEach((id) => {
+      id.hide();
+    });  
+}
+
+
+
 const handler = function() {
   return {
     //grabs the location and search query, stores them, and passes them along to API call function
