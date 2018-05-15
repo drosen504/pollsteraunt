f1ff03dbb266f267e4e8137bd99b6040357394d5
Dan Rosen <drosen84@gmail.com>
2018-05-15 15:12:37 -0500

    started writing out get endpoint for users to create poll

--- poll/routes.js
+++ poll/routes.js
@@ -2,74 +2,87 @@
 const https = require('https');
 const express = require('express');
 const router = require('router');
-const Poll = require('path/to/poll');
-// consider using async
+const Poll = require('../poll-model');
+// consider using async! in particular async.eachSeries
 
-router.get('/createpoll', function(req, res, next) {
-  if (!req.user) return res.json({err: 'not logged in'});
-  return next();
-}, function(req, res, next) {
-    var poll = new Poll({
 
-    });
-    return res.json(poll);
-});
+router.get('/pollcreate', (req, res) => {
+  const query = req.query || {};
 
-router.get('/:id', function(req, res, next) {
-    Poll.findById(id, function(err, poll) {
-        if (err) //lskdfajks
+})
 
-        // /api/polls/32104918209381209
+// router.get('/createpoll', function(req, res, next) {
+//   if (!req.user) return res.json({err: 'not logged in'});
+//   return next();
+// }, function(req, res, next) {
+//   const poll = new Poll({
 
-    })
-    // req.params.id
-});
+//   });
+//   return res.json(poll);
+// });
 
-router.post('/:id', function(req, res, next) {
-    Poll.findOneAndUpdate({_id: id}, {update_parameters}, function(err, poll) {
-        if (err) //asd
+// router.get('/:id', function(req, res, next) {
+//     Poll.findById(id, function(err, poll) {
+//         if (err) //lskdfajks
 
-    });
-    // req.params.id
-    // req.body
-});
+//         // /api/polls/32104918209381209
+
+//     })
+//     // req.params.id
+// });
+
+// router.post('/:id', function(req, res, next) {
+//     Poll.findOneAndUpdate({_id: id}, {update_parameters}, function(err, poll) {
+//         if (err) //asd
+
+//     });
+//     // req.params.id
+//     // req.body
+// });
 
 const api_key = 'wnY4Trg2G2BzWvRBHlMLl-_1KEt2Z7Hd-2D4untSR_KJ6fJYwhAlT_Y-InkvpNrfq21EKtBsJD_G2aC0DUnwQEBRO43pX4zbR39mTNe8-NFwZp_GOEF6tuDS0fLLWnYx';
-const base_uri = 'api.yelp.com';
-const path = '/v3/transactions/delivery/search?latitude=37.786882&longitude=-122.399972';
+const yelpBaseUrl = 'api.yelp.com/v3/businesses';
+const path = '';
+const zipcode = 70130;
+const restaurantCuisine = 'thai';
 
-const options = {
-  hostname: base_uri,
-  path: path,
+const yelpSearchOptions = {
+  hostname: yelpBaseUrl,
+  path: ` /search?terms=restaurants&location=${zipcode}&radius7050&limit=3&categories=${restaurantCuisine}`,
   headers: {
     Authorization: `Bearer ${api_key}`
   }
 };
 
-function makeYelpRequest(cuisine, zip) {
-  https.get(options, (res) => {
-    var str = '';
-    res.on('data', (d) => {str += d;});
+function makeYelpRequest(restaurantCuisine, zipcode) {
+  https.get(yelpSearchOptions, (res) => {
+    let string = '';
+    res.on('data', (data) => {string += data;});
     res.on('end', () => {
-      var json = JSON.parse(str);
+      let json = JSON.parse(string);
       handleJson(json);
     });
-    res.on('error', (e) => {console.error(e);});
-  }).on('error', (e) => {console.error(e);});
+    res.on('error', (err) => {console.error(err);
+    });
+  })
+    .on('error', (err) => {console.error(err);
+    });
 }
 
-function createRestaurant(yelpObject) {
+function createRestaurant(yelpObject, done) {
 
 }
 
 function createPoll(localIds) {
-    // localIds are the MongoDB ids (object._id)
-};
+  // localIds are the MongoDB ids (object._id)
+}
 
 function handleJson(json) {
-    // insert three restaurants into database
-    var localIds = [];
-    for (biz of json.businesses) {
-        createRestaurant(biz);
-    }
-}
+  // insert three restaurants into database
+  let localIds = [];
+  for (biz of json.businesses) {
+    createRestaurant(biz);
+  }
+} 
+
+module.exports = (makeYelpRequest, router);
